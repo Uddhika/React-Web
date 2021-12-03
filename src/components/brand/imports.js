adda27d9e73080865f1996ffcd103966a1ac4a70
Uddhika <uddhikaishara@gmail.com>
2021-12-03 10:29:03 +0530

    brands done

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -3,3 +3,11 @@ import slack from '../../assets/slack.png';
 import atlassian from '../../assets/atlassian.png';
 import dropbox from '../../assets/dropbox.png';
 import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
