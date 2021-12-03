26947fe3bec583bc575dda3838040df77223bb2e
Uddhika <uddhikaishara@gmail.com>
2021-12-03 11:40:55 +0530

    whatud container finished

--- src/containers/whatud/Whatud.jsx
+++ src/containers/whatud/Whatud.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatud.css';
 
 const Whatud = () => {
     return (
-        <div>
-            whatud
+        <div className="whatud__whatud section-margin">
+            <div className="whatud__whatud-feature">
+                <Feature />
+            </div>
+
+            <div className="whatud__whatud-heading">
+                <h1 className="gradient__text">The possibilties are beyond your imagination</h1>
+                <p>Explore the library</p>
+            </div>
+
+            <div className="whatud__whatud-container">
+                <Feature />
+                <Feature />
+                <Feature />
+            </div>
+
         </div>
     )
 }
