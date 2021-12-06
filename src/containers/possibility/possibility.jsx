68ce6e7fd4139241715604af94675911a9ff23a5
Uddhika <uddhikaishara@gmail.com>
2021-12-06 13:06:21 +0530

    possibility finished

--- src/containers/possibility/possibility.jsx
+++ src/containers/possibility/possibility.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './possibility.css';
 
+import possibilityImage from '../../assets/possibility.png';
+
 const possibility = () => {
     return (
         <div className="whatud__possibility section-padding" id="possibility">
-            <div className="">
-
+            <div className="whatud__possibility-image">
+                <img src={possibilityImage} alt="possibility" />
+            </div>
+            <div className="whatud__possibility-content">
+                <h4>Request Early Access to get started</h4>
+                <h1 className="gradient__text">The possibilities are beyond expectation</h1>
+                <p>Yet bed any for travelling assistance</p>
+                <h4>Request early access to get started</h4>
+            
             </div>
         </div>
     )
