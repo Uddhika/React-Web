5c5c0b0bed7cba70de176b9a6482caf3137e3c08
Uddhika <uddhikaishara@gmail.com>
2021-12-06 14:11:47 +0530

    cta completed

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const Cta = () => {
     return (
-        <div>
-            CTA
+        <div className="whatud__cta section-padding">
+            <div className="whatud__cta-content">
+                <p>Request early access to get started</p>
+                <h3>Register today and start exploring</h3>
+            </div>
+            <div className="whatud__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
         </div>
     )
 }
